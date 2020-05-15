f6cd64c2e88427d31490c646b6b886d1dfb09b01
Alejandro <alejandrofpo@gmail.com>
2020-05-15 17:21:32 +0200

    update project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('chrome-extension-test app is running!');
+    expect(page.getTitleText()).toEqual('my-profile-links app is running!');
   });
 
   afterEach(async () => {
